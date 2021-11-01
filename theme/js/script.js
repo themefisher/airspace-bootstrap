b56bb045e307ddc135d395a5d0cf9e3d991aa2bc
Sumaiya Akter <shampa.themefisher@gmail.com>
2021-11-01 13:25:14 +0600

    Updated gulpfile.js

--- theme/js/script.js
+++ theme/js/script.js
@@ -1,3 +1,10 @@
+/**
+ * WEBSITE: https://themefisher.com
+ * TWITTER: https://twitter.com/themefisher
+ * FACEBOOK: https://www.facebook.com/themefisher
+ * GITHUB: https://github.com/themefisher/
+ */
+
 $(document).ready(function () {
 	'use strict';
 
